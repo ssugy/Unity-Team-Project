fb5573ae540ffbbf3682fc584bf53658d423ca86
YHKwon <ssugy2@gmail.com>
2022-08-23 11:49:58 +0900

    #1 파일정리 및 가로세로 해상도 동적변환 적용
    고라니 영상을 통해서 동적변환 적용 완료. (https://www.youtube.com/watch?v=uQZFawccnNg)
    빌드세팅해서 정상작동함을 확임.

---
+++ TeamProject/Team Project/Assets/Scripts/CameraResolution.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ *devYH 카메라 해상도 조절하는 스크립트 (모바일 다양한 해상도 대응 용도)
+ */
+public class CameraResolution : MonoBehaviour
+{
+    public Vector2 targetRatio; // 해상도 비율
+
+    // 카메라의 screen.width height가져와서그걸 카메라 rect에 맞춘다는 의미
+    private void Awake()
+    {
+        Camera cam = GetComponent<Camera>();
+        Rect rect = cam.rect;
+        float scaleHeight = ((float)Screen.width / Screen.height) / ((float)targetRatio.x / targetRatio.y);
+        float scaleWidth = 1f / scaleHeight;
+        if (scaleHeight < 1)
+        {   
+            // 내 기준해상도 보다 더 스마트폰이 좁고 길쭉한 경우
+            rect.height = scaleHeight;
+            rect.y = (1f - scaleHeight) / 2f;
+        }
+        else
+        {
+            // 내 기준 해상도보다 스마트폰이 넓은 경우
+            rect.width = scaleWidth;
+            rect.x = (1f - scaleWidth) / 2f;
+        }
+        cam.rect = rect;
+    }
+}
