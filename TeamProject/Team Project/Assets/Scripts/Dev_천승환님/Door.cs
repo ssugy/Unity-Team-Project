74f2ace07152f3200178c9c0ce2cf47f8c744863
ehdzl411 <ehdzl411@gmail.com>
2022-09-16 21:02:04 +0900

    문열고 닫는기능에 주석추가

--- TeamProject/Team Project/Assets/Scripts/Dev_천승환님/Door.cs
+++ TeamProject/Team Project/Assets/Scripts/Dev_천승환님/Door.cs
@@ -2,33 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Door : MonoBehaviour
+
+/*public(접근제한자) : 접근제한자는 클래스 내의 멤버(필드,메서드,속성,이벤트등)의 접근을 제한하는 역할을 합니다., 클래스의 외부에서도 접근가능
+ *접근 제한자 class 고유 식별자
+ *MonoBehaviour : 유니티에서 생성하는 모든 스크립트를 상속받는 기본 클래스
+ *상속: 먼저 구현해 놓은 스크립트를 통째로 받아와서 쉽게 사용해주는 기능*/
+
+public class Door : MonoBehaviour //접근 제한자, 클래스명 : 상속     
 {
-    Transform doorPivot;
+    /*Transform : 오브젝트의 위치,회전,크기를 나타내는 컴포넌트
+     * 컴포넌트 : 프로그래밍에 있어 재사용이 가능한 각각의 독립된 모듈
+     * 모듈: 다중 파일을 다루는 일부 닷넷 바이너리
+    */
+    Transform doorPivot;         //doorPivot 트랜스폼
+
+
     public DoorButton doorButton;
-    public bool isClose;
-    // Start is called before the first frame update
+
+    /* bool: 부울값 참과 거짓을 나타내는 구조체 형식
+     * isClose : 필드 클래스 또는 구조체에 직접선언되는 모든 형식의 변수
+     */
+    public bool isClose;         //isClose 라는이름의 bool변수                             
+
+
+    /*void(반환형식) : 로컬 함수의 반환 형식으로 사용되는 메서드가 값을 반환하지않도록 지정한다.
+     * Start() : MonoBehaviour에 상속받는 스크립트 생성시에 존재하는 Start 메소드 
+     */
     void Start()
     {
-        doorPivot = transform.parent;
-        doorButton = DoorButton.instance;
+        doorPivot = transform.parent;                        //doorPivot = 부모오브젝트
+        doorButton = DoorButton.instance;                    //도어버튼을 인스턴스로 접근 어떤 스크립트에서라도 접근할수있도록 만듬
         isClose = true;
     }
 
-    // Update is called once per frame
+    /*void(반환형식) : 로컬 함수의 반환 형식으로 사용되는 메서드가 값을 반환하지않도록 지정한다.
+     * Update() : 유니티 기본함수, 누가멈추지않는이상 매프레임마다 계속 호출되는 함수
+     */
     void Update()
     {
-        
-    }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        doorButton.gameObject.SetActive(true);
-        DoorButton.door = doorPivot;
     }
+    /*private: 클래스의 내부에서만 접근가능 , 액세스중에서도 가장낮은 수준의 액서스 접근한정자를 사용하지않으면 기본값으로 private로 접근 수준이설정된다.
+     * void : 반환형식
+     * OnTriggerEnter : 게임오브젝트가 다른 게임오브젝트와 충돌하면 유니티는 OnTriggerEnter를 호출한다.
+     */
+    private void OnTriggerEnter(Collider other)               // 트리거 충돌을 이용해서 어떤 문을 열지 확인
+    {
+        doorButton.gameObject.SetActive(true);                // doorButton gameObject 활성화 
+        DoorButton.door = doorPivot;                          // 트리거충돌로 들어갈 door에 DoorButton을 활성하
+    }
+    /* OnTriggerExit : 트리거를 떠나는 모든것을 파괴시킨다.
+     * 
+     */
     private void OnTriggerExit(Collider other)
     {
-        doorButton.gameObject.SetActive(false);
-        DoorButton.door = null ;
+
+        doorButton.gameObject.SetActive(false);               //도어버튼 게임오브젝트 비활성화
+
+        DoorButton.door = null;                             //없어도 되는코드 위에 도어버튼과 짝을 맞추기위해 넣었고 혹시모를 에러를 방지하기위해 넣음
     }
 }
--- TeamProject/Team Project/Assets/Scripts/Dev_천승환님/DoorButton.cs
+++ TeamProject/Team Project/Assets/Scripts/Dev_천승환님/DoorButton.cs
@@ -4,36 +4,38 @@ using UnityEngine;
 
 public class DoorButton : MonoBehaviour
 {
-    public static DoorButton instance;
-    public static Transform door;              // 열고 닫을 문
+    /*  static: 자주변하지않는 일정한 값 혹은 설정 정보 , 인스턴스에 종속된 변수가아니라 스크립트 클래스 그 자체에 종속된 변수
+     */
+    public static DoorButton instance;         // 도어버튼에 어디서든 접근할수있도록 인스턴스선언(다른스크립트포함) 
+    public static Transform door;              // 열고 닫을 문 , 버튼이 플레이어가 문에 가까이갔을때 인식할수있도록 트랜스폼으로 지정
 
-    private void Awake()
+    private void Awake()  //모든 변수와 게임의 상태를 초기화하기 위해서 호출된다. strat보다도 먼저호출된다. 
     {
-        instance = this;
+        instance = this;         //instance : static DoorButton 변수  , this : 지금 실행중인 스크립트 
         door = null;
     }
-    void Start()
+    void Start() //
     {
-        gameObject.SetActive(false);
+        gameObject.SetActive(false);  //주어진 값에 따라 게임 오브젝트를 활성화/비활성화 합니다.
     }
 
     public void DoorOpen()
     {
-        door.rotation = door.rotation * Quaternion.Euler(0, 90f, 0);
-        door.GetComponentInChildren<Door>().isClose = false;
+        door.rotation = door.rotation * Quaternion.Euler(0, 90f, 0); //도어오픈일때 도어위치는 현재도어위치 * y값90도  
+        door.GetComponentInChildren<Door>().isClose = false; //자식door오브젝트에서 Door스크립트를 받아오고 isClose라는 bool타입변수는 Door가 열려있으므로 false로 해줘야한다. 
     }
     public void DoorClose()
     {
-        door.rotation = door.rotation * Quaternion.Euler(0, -90f, 0);
-        door.GetComponentInChildren<Door>().isClose = true;
+        door.rotation = door.rotation * Quaternion.Euler(0, -90f, 0); //도어클로즈일때 도어위치는 현재도어위치 * y값-90도
+        door.GetComponentInChildren<Door>().isClose = true; //자식door오브젝트에서 Door스크립트를 받아오고 isClose라는 bool타입변수는 Door가 닫혀있으므로 true로 해줘야한다.
     }
     public void Door()
     {
-        if (door.GetComponentInChildren<Door>().isClose)
+        if (door.GetComponentInChildren<Door>().isClose)  // 자식door오브젝트에서 Door스크립트를 받아오고 isClose가 false이면 열리고
         {
             DoorOpen();
         }
-        else
+        else                                              // true면 닫는다.
         {
             DoorClose();
         }
