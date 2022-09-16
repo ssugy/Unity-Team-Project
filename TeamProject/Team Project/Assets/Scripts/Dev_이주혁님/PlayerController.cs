1841b61ca9bb143ec8004c1cf082a5bae5956ded
Juhyeok_Lee <songari70@gmail.com>
2022-09-16 19:04:46 +0900

    #2 PlayerController 스크립트 수정. Street 에셋 박스 콜라이더 사이즈 조정. 플레이어 낙하 애니메이션 추가.
    - 기존 스크립트를 통한 이동 기능에서는 플레이어의 낙하속도가 비정상적으로 느린 현상이 있었음. PlayerController 스크립트에서 플레이어의 Velocity를 0으로 초기화한 것이 원인.
    - 스크립트를 수정하여 플레이어가 적정한 중력을 받도록 하였음. 그러나 플레이어가 지면을 밟고 있을 때, 중력을 가하면 플레이어의 Velocity를 통한 이동이 올바로 구현되지 않음. 콜라이더 충돌을 통해 플레이어가 지면을 밟고 있는지 체크하고, 지면에서 떨어져 있을 때, 즉 낙하 중일 때에만 중력을 적용받도록 함.
    - Street 에셋의 박스 콜라이더가 모델의 사이즈보다 크게 설정되어 있어 박스 콜라이더의 크기를 조정함, Village 에셋에서의 포지션도 조정하였음.
    - 플레이어가 낙하 중일 때, 낙하 애니메이션을 수행하도록 함. 지면에 닿았을 때에는 Land 애니메이션을 수행하고, 착지한 후에 플레이어가 행동할 수  있음.

--- TeamProject/Team Project/Assets/Scripts/Dev_이주혁님/PlayerController.cs
+++ TeamProject/Team Project/Assets/Scripts/Dev_이주혁님/PlayerController.cs
@@ -15,7 +15,8 @@ public class PlayerController : MonoBehaviour
     [HideInInspector] public float moveSpeed;
     [HideInInspector] public Vector3 movement;    // 플레이어의 이동 방향.
     [HideInInspector] public bool enableAct;      // 플레이어의 이동 가능 여부를 표시. (공격 기능을 구현했을 때, 이동하면서 공격할 수 없도록)
-
+    [HideInInspector] public bool isGround;
+    [HideInInspector] public float gravity;
     private void Awake()
     {
         enableAct = true;
@@ -29,7 +30,9 @@ public class PlayerController : MonoBehaviour
         playerAni = GetComponent<Animator>();
         playerJoysitck = FixedJoystick.instance;
         rotateSpeed = 5f;
-        moveSpeed = 8f;        
+        moveSpeed = 8f;
+        isGround = false;
+        gravity = 0f;
     }
 
     void PlayerMove()
@@ -40,31 +43,72 @@ public class PlayerController : MonoBehaviour
             playerJoysitck.Vertical);        
         if (movement != Vector3.zero)   // 무브먼트가 영벡터가 아닐 때 캐릭터 이동.
         {
+            if (isGround)
+            {
+                gravity = 0f;
+            }
+            else
+            {
+                gravity = -9.81f;
+            }
             Quaternion target = Quaternion.Euler(new Vector3(0, camAxis.rotation.eulerAngles.y, 0))
                 * Quaternion.LookRotation(movement);
             // 플레이어의 회전은 현재 카메라가 바라보는 방향에서 조이스틱 입력값만큼 회전한 값을 구면보간한 것.
             player.rotation = Quaternion.Slerp(player.rotation,
                 target, rotateSpeed * Time.deltaTime);
             // 리지드바디의 velocity를 조정하여 움직임을 구현함.            
-            playerrb.velocity = player.forward * movement.magnitude * moveSpeed;                      
+            playerrb.velocity = player.forward * movement.magnitude * moveSpeed + new Vector3(0, gravity, 0);            
             /** movement가 0이 아니라는 것은 플레이어가 움직인다는 뜻.
              * 따라서 플레이어의 애니메이션 상태를 전환함. */
             playerAni.SetFloat("isMove", movement.magnitude);
         }
         else if (movement == Vector3.zero)
         {
-            playerrb.velocity = Vector3.zero;
+            Vector3 tmp = playerrb.velocity;
+            tmp.x = 0f;
+            tmp.z = 0f;
+            playerrb.velocity = tmp;
+            //playerrb.velocity = Vector3.zero;
             playerAni.SetFloat("isMove", 0f);
         }        
         
     }
     
-    void Update()
+    void FixedUpdate()
     {        
         if (enableAct)  // 공격 중일 땐 이동을 못하게 함.
         {
             PlayerMove();
         }
-        camAxis.position = player.position; // 카메라 중심 축이 플레이어 포지션을 따라다니도록 함.        
+        camAxis.position = player.position; // 카메라 중심 축이 플레이어 포지션을 따라다니도록 함.
+                                            
+        if (playerrb.velocity.y < -9.5f)
+        {
+            Vector3 tmp = playerrb.velocity;
+            tmp.y = -9.5f;
+            playerrb.velocity = tmp;
+        }
     }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.transform.tag.Equals("Terrain"))
+        {
+            isGround = false;
+            Invoke("Fall", 0.2f);
+        }
+    }    
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.transform.tag.Equals("Terrain"))
+        {
+            playerAni.SetBool("isGround", true);            
+            isGround = true;
+            CancelInvoke("Fall");            
+        }
+    }
+    public void Fall()
+    {
+        playerAni.SetBool("isGround", false);
+    }
+
 }
