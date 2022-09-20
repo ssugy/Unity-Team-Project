e8463edb646c6e4e478b91e1c6a716de7dd194b4
YHKwon <ssugy2@gmail.com>
2022-09-20 16:52:01 +0900

    #1 빌드시 오류 발견되어서, 사용하지 않는 namespace제거

--- TeamProject/Team Project/Assets/Scripts/공통부분_Singleton Manager/AudioManager.cs
+++ TeamProject/Team Project/Assets/Scripts/공통부분_Singleton Manager/AudioManager.cs
@@ -1,9 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using TreeEditor;
-using Unity.VisualScripting;
-using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.UI;
 
