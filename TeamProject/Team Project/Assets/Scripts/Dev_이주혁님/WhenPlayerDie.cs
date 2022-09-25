87baff2755e3b2eb12a03b9e1124621858088a72
Juhyeok_Lee <songari70@gmail.com>
2022-09-25 23:04:16 +0900

    #2 Female 애니메이션 컨트롤러 수정. Female 프리팹에 CharacterController, PlayerInventory 스크립트 추가.
    - Male 프리팹과 동일하게 기능할 수 있도록 애니메이션 컨트롤러와 스크립트를 추가하였음.

--- TeamProject/Team Project/Assets/Scripts/Dev_이주혁님/WhenPlayerDie.cs
+++ TeamProject/Team Project/Assets/Scripts/Dev_이주혁님/WhenPlayerDie.cs
@@ -9,7 +9,7 @@ public class WhenPlayerDie : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Camera.main.transform.parent.rotation = Quaternion.Lerp(Camera.main.transform.parent.rotation, Quaternion.Euler(90f, 0, 0), 2 * Time.deltaTime);
+        Camera.main.transform.parent.rotation = Quaternion.Lerp(Camera.main.transform.parent.rotation, Quaternion.Euler(90f, Camera.main.transform.parent.rotation.eulerAngles.y, 0), 2 * Time.deltaTime);
         Camera.main.transform.localPosition = Vector3.Lerp(Camera.main.transform.localPosition, new Vector3(0, 0, -10f), Time.deltaTime);
     }
 }
