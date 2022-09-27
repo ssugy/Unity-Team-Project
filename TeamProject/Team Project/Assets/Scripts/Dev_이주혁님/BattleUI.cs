31d08f5d9557c8b94c2801f930c3887b6cfe57e2
Juhyeok_Lee <songari70@gmail.com>
2022-09-27 11:02:53 +0900

    #2 전투 UI에서 버튼을 클릭했을 떄 버튼의 색이 바뀌도록 수정하였음.
    - 버튼을 눌린 것을 시인성 있게 확인할 수 있도록 공격/구르기/방어 버튼이 눌렸을 때 스프라이트가 바뀌게 하였음. 메뉴 버튼 등은 스프라이트를 바꾸지 않고 컬러만 조정함.

--- TeamProject/Team Project/Assets/Scripts/Dev_이주혁님/BattleUI.cs
+++ TeamProject/Team Project/Assets/Scripts/Dev_이주혁님/BattleUI.cs
@@ -15,6 +15,8 @@ public class BattleUI : MonoBehaviour
     public EventTrigger lArm;           // lArm 기능은 버튼이 아닌 이벤트트리거를 사용함.
     private EventTrigger.Entry lArm_PointerDown;
     private EventTrigger.Entry lArm_PointerUp;
+    public Sprite upFrame;
+    public Sprite downFrame;
 
     // Start is called before the first frame update
     void Start()
@@ -28,12 +30,25 @@ public class BattleUI : MonoBehaviour
         lArm_PointerDown = new EventTrigger.Entry();
         lArm_PointerDown.eventID = EventTriggerType.PointerDown;
         lArm_PointerDown.callback.AddListener((data) => { playerController.LArmDown((PointerEventData)data); });
+        lArm_PointerDown.callback.AddListener((data) => { LArmDown_Frame((PointerEventData)data); });
         lArm_PointerUp = new EventTrigger.Entry();
         lArm_PointerUp.eventID = EventTriggerType.PointerUp;
         lArm_PointerUp.callback.AddListener((data) => { playerController.LArmUp((PointerEventData)data); });
+        lArm_PointerUp.callback.AddListener((data) => { LArmUp_Frame((PointerEventData)data); });
         lArm.triggers.Add(lArm_PointerDown);
         lArm.triggers.Add(lArm_PointerUp);
 
+    }
+
+    void LArmDown_Frame(PointerEventData data)
+    {
+        Image frame = lArm.transform.GetComponent<Image>();
+        frame.sprite = downFrame;
+    }
+    void LArmUp_Frame(PointerEventData data)
+    {
+        Image frame = lArm.transform.GetComponent<Image>();
+        frame.sprite = upFrame;
     }
 
     // Update is called once per frame
