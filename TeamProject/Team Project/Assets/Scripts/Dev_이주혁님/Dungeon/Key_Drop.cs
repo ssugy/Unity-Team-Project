d5b29f035a3378183034a0fb757f6aed497c7b0f
Juhyeok_Lee <songari70@gmail.com>
2022-10-14 14:50:13 +0900

    #2 플레이어 캐릭터의 스킬 사용 후 선입력된 공격 트리거에 의해 의도치 않은 공격이 나가는 현상 수정. 던전의 용 몬스터가 열쇠를 드랍하지 않는 버그 수정.

--- TeamProject/Team Project/Assets/Scripts/Dev_이주혁님/Dungeon/Key_Drop.cs
+++ TeamProject/Team Project/Assets/Scripts/Dev_이주혁님/Dungeon/Key_Drop.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class Key_Drop : MonoBehaviour
 {
+    public Enemy enemy;
     public Door door;      // 해당 열쇠가 열 문.
     public GameObject message;
 
-    
+    private void Start()
+    {
+        enemy = GetComponent<Enemy>();
+        
+    }
+
     public void PickUpKey()
     {
         message.SetActive(true);
         door.isLocked = false;        
+        
+    }
+    private void Update()
+    {
+        if (enemy.curHealth <= 0)
+        {
+            PickUpKey();
+        }
     }
 }
