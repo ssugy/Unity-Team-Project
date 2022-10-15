60f810509a21a880f3abe3821521287cbe4f7ee4
jooyoung cha <ryuha.kuma@gmail.com>
2022-10-15 19:06:21 +0900

    #2 컷신 카메라 위치 관련 조정 완료/사운드 페이드인 적용

--- TeamProject/Team Project/Assets/Scripts/Dev_차주영님/JY_CutScenePlay.cs
+++ TeamProject/Team Project/Assets/Scripts/Dev_차주영님/JY_CutScenePlay.cs
@@ -9,6 +9,7 @@ public class JY_CutScenePlay : MonoBehaviour
     public GameObject CutScene2;
     public GameObject BattleUI;
     public Camera mainCam;
+    public Camera cutSceneCam;
     public Canvas fadeUI;
     public Image fade;
     public float fadeTime;
@@ -28,32 +29,34 @@ public class JY_CutScenePlay : MonoBehaviour
             Player.instance.movement = Vector3.zero;
             StartCoroutine("CutScene_1");
             firstPlay = false;
-            CamAxisPos = mainCam.transform.parent.transform.position;
-            CamPos = mainCam.transform.position;
         }
 
     }
     IEnumerator CutScene_1()
     {
-
+        //시작 페이드아웃
         StartCoroutine(Fade(0, 1));
-        StartCoroutine("AudioDestroy");
+        AudioManager.s_instance.SoundFadeInOut(AudioManager.SOUND_NAME.BGM, 0, 2);
         yield return new WaitForSeconds(2f);
+        //카메라 전환
+        mainCam.gameObject.SetActive(false);
+        cutSceneCam.gameObject.SetActive(true);
+        //UI 전환 및 페이드 인, 컷신 재생, BGM 재생
         BattleUI.SetActive(false);
         StartCoroutine(Fade(1, 0));
         CutScene1.SetActive(true);
         yield return new WaitForSeconds(5f);
         AudioManager.s_instance.SoundPlay(AudioManager.SOUND_NAME.BossBGM_01, true);
         yield return new WaitForSeconds(5f);
-
+        //컷신 종료 및 페이드 아웃, 카메라 전환
         StartCoroutine(Fade(0, 1));
         yield return new WaitForSeconds(2f);
         CutScene1.SetActive(false);
-        yield return new WaitForSeconds(2f);
+        cutSceneCam.gameObject.SetActive(false);
+        mainCam.gameObject.SetActive(true);
+        yield return new WaitForSeconds(1f);
         Player.instance.enableMove = true;
         Player.instance.movement = Vector3.zero;
-        mainCam.transform.parent.transform.position = CamAxisPos;
-        mainCam.transform.position = CamPos;
         yield return new WaitForSeconds(1f);
         StartCoroutine(Fade(1, 0));
         BattleUI.SetActive(true);
@@ -92,10 +95,4 @@ public class JY_CutScenePlay : MonoBehaviour
             fadeUI.gameObject.SetActive(false);
     }
     
-    IEnumerator AudioDestroy()
-    {
-        AudioSource target = AudioManager.s_instance.GetComponentInChildren<AudioSource>();
-        Destroy(target.gameObject);
-        yield return null;
-    }
 }
