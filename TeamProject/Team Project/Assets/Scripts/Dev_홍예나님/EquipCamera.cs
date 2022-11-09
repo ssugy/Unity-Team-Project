ac6b7856ccb1546521b204106ab2237a6b06ba8e
giraffe09 <yenahong84@gmail.com>
2022-11-09 23:16:14 +0900

    #2 장비 UI 구성 완료, 상정 UI 임시 구성. 인벤토리-장비창에서 캐릭터의 장비 장착 상태를 확인 할 수 있음.

---
+++ TeamProject/Team Project/Assets/Scripts/Dev_홍예나님/EquipCamera.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipCamera : MonoBehaviour
+{
+    private GameObject player;
+    private Vector3 initPos;
+    public float distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initPos = transform.position;
+        FindPlayer();
+    }
+
+    void FindPlayer()
+    {
+        player = Player.instance.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FindPlayer();
+        if (player != null)
+        {
+            Vector3 rot = player.transform.rotation.eulerAngles;
+            float y = - rot.y + 90f;
+            Vector3 pos = player.transform.position + new Vector3(distance * Mathf.Cos(y * Mathf.Deg2Rad), initPos.y, distance * Mathf.Sin(y * Mathf.Deg2Rad));
+            Vector3 rotCam = new Vector3(0, 270.0f - y, 0);
+            transform.position = pos;
+            transform.rotation = Quaternion.Euler(rotCam);
+        }
+    }
+}
