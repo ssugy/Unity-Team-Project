c8454884f82fe1a9c35b1548e29ec21040830924
jooyoung cha <ryuha.kuma@gmail.com>
2022-11-14 01:34:15 +0900

    #2 보스 부위파괴 관련 히트박스 추가

--- TeamProject/Team Project/Assets/Scripts/Dev_차주영님/Player/JY_PartDestruction.cs
+++ TeamProject/Team Project/Assets/Scripts/Dev_차주영님/Player/JY_PartDestruction.cs
@@ -31,21 +31,6 @@ public class JY_PartDestruction : MonoBehaviour, IPointerDownHandler, IPointerUp
             PartDestrcutionCam.gameObject.transform.position = Player.instance.transform.position + new Vector3(0,2f,4f);
         }
     }
-    void Rotate()
-    {
-        rotateX += dragOn.xAngle * 12f;
-        rotateY += dragOn.yAngle * -1.1f;           // 드래그 방향과 카메라 회전 방향을 맞추기 위해 -1을 곱함.
-        dragOn.xAngle = 0;
-        dragOn.yAngle = 0;
-        if (rotateY > 1.3f) rotateY = 1.3f;          // 카메라가 너무 많이 회전하는 것을 방지하는 코드.
-        if (rotateY < -0.3f) rotateY = -0.3f;
-        /** 카메라의 중심축을 화면을 드래그한 값만큼 회전시킴.
-         * 카메라는 중심축의 자식 오브젝트이므로 함께 회전함. */
-        PartDestrcutionCam.transform.rotation = Quaternion.Euler(new Vector3(
-            PartDestrcutionCam.transform.rotation.x + rotateY,
-            PartDestrcutionCam.transform.rotation.y + rotateX, 0f) * camSpeed);
-    }
-
     public void OnPointerDown(PointerEventData eventData)
     {
         isShoot = true;
