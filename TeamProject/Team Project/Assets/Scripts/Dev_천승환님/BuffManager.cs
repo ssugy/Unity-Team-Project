c8a1a35ad542392b55f736321aabfae16e649561
ehdzl411 <ehdzl411@gmail.com>
2022-11-17 21:23:31 +0900

    버프랜덤위치생성 및 먹기

--- TeamProject/Team Project/Assets/Scripts/Dev_천승환님/BuffManager.cs
+++ TeamProject/Team Project/Assets/Scripts/Dev_천승환님/BuffManager.cs
@@ -3,37 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+
+
 public class BuffManager : MonoBehaviour
 {
+    
     public Coordinate[] tmp =
     {
-        new Coordinate(3.8,-2.87,46.94),
-        new Coordinate(-3.47,-2.89,46.93),
-        new Coordinate(-16.04,-2.87,18.78),
-        new Coordinate(30.01,-6.915,20.94),
-        new Coordinate(17.97,-6.89,85.4),
-        new Coordinate(51.9,-2.73,67.43),
-        new Coordinate(8.66,1.56,101.18),
-        new Coordinate(77.19,1.26,101.31),
+        new Coordinate(3.8f,-2.87f,46.94f),
+        new Coordinate(-3.47f,-2.89f,46.93f),
+        new Coordinate(-16.04f,-2.87f,18.78f),
+        new Coordinate(30.01f,-6.915f,20.94f),
+        new Coordinate(17.97f,-6.89f,85.4f),
+        new Coordinate(51.9f,-2.73f,67.43f),
+        new Coordinate(8.66f,1.56f,101.18f),
+        new Coordinate(77.19f,1.26f,101.31f),
     };
     public List<Coordinate> Coordinates = new();
+    
 
-    //public GameObject[] Buff_Prefabs;
+    public GameObject[] Buff_Prefabs;
 
     
     void Start()
     {
+        
         //SpawnBuff(Buff_Prefabs[0], new Vector3(1, 0, 0));
         //SpawnBuff(Buff_Prefabs[1], new Vector3(-1, 0, 0));
-        Coordinates.RemoveAt(Random.Range(0, Coordinates.Count));
-        Coordinates.RemoveAt(Random.Range(0, Coordinates.Count));
         for (int i = 0; i < tmp.Length; i++)
-        Coordinates.Add(tmp[i]);
-        for(int i = 0; i < 6; i++)
         {
-            //GameObject buff_Prefabs = Buff_Prefabs[i];
-            //Vector3 buff_Position = Coordinates[Random.Range(0, Coordinates.Length)].GetPosition();
-            //SpawnBuff(buff_Prefabs, buff_Position);
+            Coordinates.Add(tmp[i]);
+        }
+
+        
+
+        for (int i = 0; i < 6; i++)
+        
+        {
+            int tmp = Random.Range(0, Coordinates.Count);
+            SpawnBuff(Buff_Prefabs[i], Coordinates[tmp].GetPosition());
+            Coordinates.RemoveAt(tmp);
         }
     }
 
@@ -45,21 +54,21 @@ public class BuffManager : MonoBehaviour
 
     public struct Coordinate
     {
-        public double x;
-        public double y;
-        public double z;
+        public float x;
+        public float y;
+        public float z;
 
-        public Coordinate(double x, double y, double z)
+        public Coordinate(float x, float y, float z)
         {
             this.x = x;
             this.y = y;
             this.z = z;
         }
 
-        /*public Vector3 GetPosition()
+        public Vector3 GetPosition()
         {
             return new Vector3(x, y, z);
             
-        }*/
+        }
     }
 }
