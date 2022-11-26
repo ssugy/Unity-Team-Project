a19ee2556358f230f1e3c45fae4c6687b5877265
jooyoung cha <ryuha.kuma@gmail.com>
2022-11-26 19:39:06 +0900

    #2 포탈에 투사체 발사시 포탈 UI가 나오는 현상 조치

--- TeamProject/Team Project/Assets/Scripts/Dev_차주영님/JY_Portal.cs
+++ TeamProject/Team Project/Assets/Scripts/Dev_차주영님/JY_Portal.cs
@@ -11,8 +11,11 @@ public class JY_Portal : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         // 포탈에 접촉한 게 자신일 때만 UI를 활성화. 혹은 마을에서는 플레이어가 자신 하나뿐이므로 패널 활성화.
-        if(GameManager.s_instance.currentScene.Equals(GameManager.SceneName.World) || other.GetComponent<Player>().photonView.IsMine )
-            portalPanel.gameObject.SetActive(true);
+        if (other.CompareTag("Player"))
+        {
+            if (GameManager.s_instance.currentScene.Equals(GameManager.SceneName.World) || other.GetComponent<Player>().photonView.IsMine)
+                portalPanel.gameObject.SetActive(true);
+        }
     }
     private void OnTriggerExit(Collider other)
     {
