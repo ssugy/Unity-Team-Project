065c3696a65a71118bd63dbaed78ac6858d2a429
jooyoung cha <ryuha.kuma@gmail.com>
2022-12-07 22:58:54 +0900

    #2 화염던전 가이드 기능 구현
    - 목표 방향을 계산하고 바라보도록 구현 완료
    - 2,3,4 단계만 적용하면 됨

---
+++ TeamProject/Team Project/Assets/Scripts/Dev_차주영님/Player/JY_Guide.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JY_Guide : MonoBehaviour
+{
+    public Transform target_1;
+    Player player;
+    int state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = JY_CharacterListManager.s_instance.playerList[0];
+        state = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        calcVec(target_1);
+    }
+    void calcVec(Transform target)
+    {
+        Vector3 dir = (target.position-player.transform.position).normalized;
+        dir.y = 0f;
+        Vector3 tmp = dir + player.transform.position;
+        tmp.y += 0.5f;
+        transform.position = tmp;
+        transform.LookAt(target);
+    }
+}
