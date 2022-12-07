c00c2ba07bb1187ff45376b46d70f5134779793d
Juhyeok_Lee <songari70@gmail.com>
2022-12-07 23:43:14 +0900

    #2 상점 기능 임시 구현 완료.

--- TeamProject/Team Project/Assets/Scripts/Dev_이주혁님/NPCs/Shop.cs
+++ TeamProject/Team Project/Assets/Scripts/Dev_이주혁님/NPCs/Shop.cs
@@ -1,18 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Shop : MonoBehaviour
 {
+    public static Shop shop;
+    public Item selected;
+    public int price;
+
+    public GameObject infoPanel;
+    public Image infoIcon;
+    public Text infoName;
+    public Text infoType;
+    public Text infoLevel;
+    public Text infoExplain;
+
+    public Button buy;
+    public Text goldText;
+    private void OnEnable()
+    {
+        goldText.text = JY_CharacterListManager.s_instance.playerList[0].playerStat.Gold.ToString();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        shop = this;
+        buy.onClick.RemoveAllListeners();
+        buy.onClick.AddListener(() => BuyItem());
+    }    
+    public void UpdatePanel()
+    {
+        infoPanel.SetActive(true);
+        infoIcon.sprite = selected.image;
+        infoName.text = selected.name;
+        switch (selected.type)
+        {
+            case ItemType.EQUIPMENT:
+                {
+                    infoType.text = "장비";
+                    break;
+                }
+            case ItemType.CONSUMABLE:
+                {
+                    infoType.text = "소비";
+                    break;
+                }
+            case ItemType.INGREDIENTS:
+                {
+                    infoType.text = "재료";
+                    break;
+                }
+        }
+        infoLevel.text = "Lv. " + selected.level.ToString();
+        infoExplain.text = selected.explanation;
     }
 
-    // Update is called once per frame
-    void Update()
+    // 구매 버튼에 할당될 메소드.
+    public void BuyItem()
     {
-        
+        // 플레이어의 소지금이 아이템 가격보다 적은 지 체크. 소지금이 적다면 리턴.
+        if (JY_CharacterListManager.s_instance.playerList[0].playerStat.Gold < price)
+            return;
+
+        // 아이템 추가에 성공했으면 (플레이어의 인벤토리가 가득 차지 않았다면)
+        if (JY_CharacterListManager.s_instance.playerList[0].GetComponent<Inventory>().AddItem(selected))
+        {
+            Debug.Log("구매 성공");
+            JY_CharacterListManager.s_instance.playerList[0].playerStat.Gold -= price;
+            goldText.text = JY_CharacterListManager.s_instance.playerList[0].playerStat.Gold.ToString();
+            return;
+        }
+        Debug.Log("구매 실패");
     }
 }
--- TeamProject/Team Project/Assets/Scripts/Dev_이주혁님/NPCs/Slot_Shop.cs
+++ TeamProject/Team Project/Assets/Scripts/Dev_이주혁님/NPCs/Slot_Shop.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Slot_Shop : MonoBehaviour
 {
+    // 판매할 아이템의 번호.
+    public int index;
+    public int price;
+
+    Item item;
+    public Image icon;
+    public Text nameText;
+    public Text priceText;
+       
+    Button mine;
+
     // Start is called before the first frame update
     void Start()
     {
+        item = ItemDatabase.s_instance.itemDB[index].Copy();
+        icon.sprite = item.image;
+        nameText.text = item.name;
+        priceText.text = price.ToString();
+
         
+        mine = GetComponent<Button>();
+        mine.onClick.AddListener(() => OnClick());
     }
 
-    // Update is called once per frame
-    void Update()
+    // 슬롯 클릭 시 실행될 메소드.
+    public void OnClick()
     {
-        
+        Shop.shop.selected = item.Copy();
+        Shop.shop.price = price;
+        Shop.shop.UpdatePanel();
     }
 }
